cc4c124d393c603e5b76cd5f453d699f507856d7
miguel <jmiguelhsg@gmail.com>
2025-04-04 20:19:22 -0300

    feat: Display Camion list and add UI for inserting
    This commit introduces the ability to display a list of `Camion` objects in the `CamionScreen` and adds a button to trigger the insertion of a new `Camion`.
    
    -   **Camion List Display:**
        -   Implemented `ListOfCamionesScreen` composable to display a list of `Camion` objects fetched from the `CamionViewModel`.
        -   Created `CamionCard` composable to render individual `Camion` objects with their `id`, `choferName`, `patenteTractor`, and `patenteJaula`.
        -   Used `LazyColumn` for efficient rendering of the `Camion` list.
    -   **Insert Camion Button:**
        -   Added a button in `CamionScreen` that, when clicked, calls the `insertCamion` function in `CamionViewModel` to add a new `Camion`.
    - **Refactor** `CamionScreen`
        - `CamionScreen`  now uses `ListOfCamionesScreen` and `CamionCard` composables to display the list of `Camiones` in a card view, and remove `InserCamionScreen`
    - **Update** `Camion` model
        - Add `toString` method in model.

--- app/src/main/java/com/example/fletes/ui/camion/CamionScreen.kt
+++ app/src/main/java/com/example/fletes/ui/camion/CamionScreen.kt
@@ -1,42 +1,79 @@
 package com.example.fletes.ui.camion
 
 import android.util.Log
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.fletes.data.room.Camion
 
 @Composable
 fun CamionScreen(viewModel: CamionViewModel) {
     val camiones = viewModel.camiones.collectAsStateWithLifecycle()
-    Scaffold { paddingValues ->
-        LazyColumn {
-            items(camiones.value.size) { index ->
-                Text(text = camiones.value[index].toString())
-            }
+    Scaffold(
+        content = {
+            ListOfCamionesScreen(
+                camiones = camiones.value,
+                onInsertCamion = { viewModel.insertCamion() },
+                modifier = Modifier.padding(it)
+            )
         }
-        InserCamionScreen(paddingValues) {
-            viewModel.insertCamion()
-        }
-    }
+    )
 }
 
-@Composable
-fun InserCamionScreen(paddingValues: PaddingValues, onInsertCamion: () -> Unit) {
 
+@Composable
+fun ListOfCamionesScreen(
+    camiones: List<Camion>,
+    onInsertCamion: () -> Unit,
+    modifier: Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        LazyColumn(modifier = modifier) {
+            items(camiones.size) { index ->
+                CamionCard(camion = camiones[index])
+            }
+        }
+    }
     Button(
         onClick = {
             onInsertCamion()
             Log.d("CamionViewModel", "Camion inserted")
         },
-        modifier = Modifier.padding(paddingValues)
+        modifier = Modifier
     ) {
         Text(text = "Insertar Camion")
     }
 
+}
+
+@Composable
+fun CamionCard(camion: Camion) {
+    Card(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "ID: ${camion.id}", style = MaterialTheme.typography.bodyMedium)
+            Text(text = "Chofer: ${camion.choferName}", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = "Patente Tractor: ${camion.patenteTractor}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "Patente Jaula: ${camion.patenteJaula}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
 }
